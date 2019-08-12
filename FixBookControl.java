1c85344f7b80ffbceefc83aed90a57930af3f78d
PrabashiMithma <40761612+PrabashiMithma@users.noreply.github.com>
2019-08-12 10:36:36 +1000

    Change variable
    Change variable StAtE to state Author Prabashi

--- FixBookControl.java
+++ FixBookControl.java
@@ -2,30 +2,30 @@ public class FixBookControl {
 	
 	private FixBookUI UI;
 	private enum CONTROL_STATE { INITIALISED, READY, FIXING };
-	private CONTROL_STATE StAtE;
+	private CONTROL_STATE state; //Change StAtE to state
 	
-	private library LIB;
-	private book Cur_Book;
+	private library lib;// Change LIB to lib Author Prabashi
+	private book cur_Book; // Change cur_Book  Author Prabashi
 
 
 	public FixBookControl() {
-		this.LIB = LIB.INSTANCE();
-		StAtE = CONTROL_STATE.INITIALISED;
+		this.lib = lib.INSTANCE();  // change LIB to lib Author Prabashi
+		state = CONTROL_STATE.INITIALISED;//Change StAtE to state
 	}
 	
 	
 	public void Set_Ui(FixBookUI ui) {
-		if (!StAtE.equals(CONTROL_STATE.INITIALISED)) {
+		if (!state.equals(CONTROL_STATE.INITIALISED)) { //Change StAtE to state Author Prabashi
 			throw new RuntimeException("FixBookControl: cannot call setUI except in INITIALISED state");
 		}	
 		this.UI = ui;
 		ui.Set_State(FixBookUI.UI_STATE.READY);
-		StAtE = CONTROL_STATE.READY;		
+		state = CONTROL_STATE.READY;	//Change StAtE to state	Author Prabashi
 	}
 
 
 	public void Book_scanned(int bookId) {
-		if (!StAtE.equals(CONTROL_STATE.READY)) {
+		if (!state.equals(CONTROL_STATE.READY)) { //Change StAtE to state Author Prabashi
 			throw new RuntimeException("FixBookControl: cannot call bookScanned except in READY state");
 		}	
 		Cur_Book = LIB.Book(bookId);
@@ -40,12 +40,12 @@ public class FixBookControl {
 		}
 		UI.display(Cur_Book.toString());
 		UI.Set_State(FixBookUI.UI_STATE.FIXING);
-		StAtE = CONTROL_STATE.FIXING;		
+		state = CONTROL_STATE.FIXING;	//Change StAtE to state	Author Prabashi	
 	}
 
 
 	public void FIX_Book(boolean MUST_fix) {
-		if (!StAtE.equals(CONTROL_STATE.FIXING)) {
+		if (!state.equals(CONTROL_STATE.FIXING)) { //Change StAtE to state	Author Prabashi
 			throw new RuntimeException("FixBookControl: cannot call fixBook except in FIXING state");
 		}	
 		if (MUST_fix) {
@@ -53,12 +53,12 @@ public class FixBookControl {
 		}
 		Cur_Book = null;
 		UI.Set_State(FixBookUI.UI_STATE.READY);
-		StAtE = CONTROL_STATE.READY;		
+		state = CONTROL_STATE.READY;		
 	}
 
 	
 	public void SCannING_COMplete() {
-		if (!StAtE.equals(CONTROL_STATE.READY)) {
+		if (!state.equals(CONTROL_STATE.READY)) {
 			throw new RuntimeException("FixBookControl: cannot call scanningComplete except in READY state");
 		}	
 		UI.Set_State(FixBookUI.UI_STATE.COMPLETED);		
